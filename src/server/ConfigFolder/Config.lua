81ea8ad8481d21b24daf03661fa4b3e9fb806243
Lavelius <jonathansirmans29@gmail.com>
2025-09-14 19:45:12 -0700

    updated cannon model but there are some known issues 1. upgrading 1 cannon upgrades both 2. need random angular velocity upon fire

--- src/server/ConfigFolder/Config.lua
+++ src/server/ConfigFolder/Config.lua
@@ -6,7 +6,7 @@ return {
 	Cannon = {
 		CooldownSec = 1.0,
 		LocalRotationDeg = { x = 90, y = 180, z = 90 },
-		LocalPositionOffset = { x = 8, y = 0, z = 2 },
+		LocalPositionOffset = { x = 5, y = 0, z = 4 },
 		ClickDistance = 64,
 	},
 	Dice = {
@@ -16,5 +16,9 @@ return {
 		MaxWait = 8.0, -- fallback: auto-score after this many seconds
 		PopupLife = 1.2, -- seconds to show +X over the die
 		DespawnAfterScore = 0.8, -- NEW: seconds after awarding to delete the die
+	},
+	Upgrades = {
+		DiceSidesOrder = { 4, 6, 8, 10, 12, 20 }, -- progression path
+		DiceSidesCost = { [6] = 100, [8] = 250, [10] = 500, [12] = 1000, [20] = 2000 }, -- price to reach that tier
 	},
 }
