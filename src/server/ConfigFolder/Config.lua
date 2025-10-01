e8756e66fa9aee7ccbe4847a244832dd04f123e5
Lavelius <jonathansirmans29@gmail.com>
2025-09-30 21:15:22 -0700

    Halfway through changes. UH OH.

--- src/server/ConfigFolder/Config.lua
+++ src/server/ConfigFolder/Config.lua
@@ -1,10 +1,10 @@
 return {
 	Costs = {
 		TycCannon1 = 0,
-		TycCannon2 = 50,
+		TycCannon2 = 10000,
 	},
 	Cannon = {
-		CooldownSec = 1.0,
+		CooldownSec = 2.0,
 		LocalRotationDeg = { x = 90, y = 180, z = 90 },
 		LocalPositionOffset = { x = 5, y = 0, z = 4 },
 		ClickDistance = 64,
@@ -16,9 +16,5 @@ return {
 		MaxWait = 8.0, -- fallback: auto-score after this many seconds
 		PopupLife = 1.2, -- seconds to show +X over the die
 		DespawnAfterScore = 0.8, -- NEW: seconds after awarding to delete the die
-	},
-	Upgrades = {
-		DiceSidesOrder = { 4, 6, 8, 10, 12, 20 }, -- progression path
-		DiceSidesCost = { [6] = 100, [8] = 250, [10] = 500, [12] = 1000, [20] = 2000 }, -- price to reach that tier
 	},
 }
